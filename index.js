57ab252399c436a60c42ab7c31b9ccc85b2a59e7
n.makovsky <phpy@mail.ru>
2017-09-09 15:55:14 +0300

    Первоначальный коммит

---
+++ index.js
@@ -0,0 +1,58 @@
+var express = require('express');
+
+var app = express();
+
+// Установка механизма представления handlebars
+var handlebars = require('express-handlebars').create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+
+app.set('port', process.env.PORT || 3000);
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+var fortunes = [
+	"Conquer your fears or they will conquer you.",
+	"Rivers need springs.",
+	"Do not fear what you don't know.",
+	"You will have a pleasant surprise.",
+	"Whenever possible, keep it simple.",
+];
+
+
+
+app.get('/', function(req, res){
+	res.render('home');
+
+})
+
+app.get('/about', function(req, res){
+	var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)]
+	res.render('about', { fortune : randomFortune });
+})
+
+
+
+// userpage404
+app.use( function(req, res){
+	res.status(404);
+	res.render('404')
+})
+
+
+
+// userpage500
+app.use( function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500')
+})
+
+
+// слушаем
+app.listen(app.get('port'), function(){
+	console.log('Екчпаресс запущен на http://localhost:' + app.get('port'))
+})
